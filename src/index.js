bf10107250b83950fe0828e0e971e8b63b694430
SekiguchiKai <sekiguchikai@SekiguchiKai-1.local>
2017-02-02 13:15:29 +0900

    すでに打ち手が置いてあるところにおくことができなくなった

--- src/index.js
+++ src/index.js
@@ -2,40 +2,40 @@ import Board from './board.js';
 import Cpu from './cpu.js';
 import Judge from './judge.js';
 
+const board = new Board(3, 3);
+const cpu = new Cpu('cpu', '×', board);
+const judge = new Judge(3, 3, 3);
 
-class Handler {
+let idArray = ['0-0', '0-1', '0-2', '1-0', '1-1', '1-2', '2-0', '2-1', '2-2'];
 
-    constructor() {
-        this.board = new Board(3, 3);
-        this.cpu = new Cpu('cpu', '×', board);
-        this.judge = new Judge(3, 3, 3);
-
-    }
-
-    init() {
-        let idArray = ['0-0', '0-1', '0-2', '1-0', '1-1', '1-2', '2-0', '2-1', '2-2'];
-
-        for (let id of idArray) {
-            let e = document.getElementById(id);
-            e.addEventListener('click', () => {
-                let rowColumn = id.split('-');
-                let row = Number(rowColumn[0]);
-                let column = Number(rowColumn[1]);
-
-                board.addMove(row, column, '○');
-                e.textContent = board.getMove(row, column);
-
-                let depth = 2;
-
-                cpu.doMove(depth, board);
-
-                judge.judgeResult(board);
+for (let id of idArray) {
 
 
-            });
+    // if (judge.judgeResult(board) != '未決') { break; };
+
+
+    let e = document.getElementById(id);
+    e.addEventListener('click', () => {
+        let rowColumn = id.split('-');
+        let row = Number(rowColumn[0]);
+        let column = Number(rowColumn[1]);
+
+        let cellMove = board.getMove(row, column);
+
+        if (cellMove === ' ') {
+            board.addMove(row, column, '○');
+            e.textContent = board.getMove(row, column);
+
+            let depth = 2;
+
+            cpu.doMove(depth, board);
+
+            judge.judgeResult(board);
+        } else {
+            window.alert('石はすでに置いてある! 他のところを選んで!');
         }
-    }
-}
+    });
 
+}
 
 
