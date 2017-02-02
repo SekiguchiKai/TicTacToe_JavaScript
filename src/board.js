9ec96db0ff0e54a719211a518af6e819c4535733
SekiguchiKai <sekiguchikai@SekiguchiKai-1.local>
2017-02-02 17:39:01 +0900

    Boardクラスのフィールドをprivate化

--- src/board.js
+++ src/board.js
@@ -1,47 +1,48 @@
+
+// フィールドをprivate化するための処理
+let _rowSize = new WeakMap();
+let _columnSize = new WeakMap();
+let _gameBoard = new WeakMap();
+
+
 export default class Board {
 
     constructor(rowSize, columnSize) {
-        this.rowSize = rowSize;
-        this.columnSize = columnSize;
-        this.gameBoard = new Array(3);
-        for (let row = 0; row < this.gameBoard.length; row++) {
-            this.gameBoard[row] = new Array(3);
+        // WeakMapに対応づけ
+        _rowSize.set(this, rowSize);
+        _columnSize.set(this, columnSize);
+        _gameBoard.set(this, new Array(3));
+
+        for (let row = 0; row < _gameBoard.get(this).length; row++) {
+            _gameBoard.get(this)[row] = new Array(3);
         }
 
         // 二次元配列初期化
-        for (let row = 0; row < this.rowSize; row++) {
-            for (let column = 0; column < this.columnSize; column++) {
-                this.gameBoard[row][column] = ' ';
+        for (let row = 0; row < _rowSize.get(this); row++) {
+            for (let column = 0; column < _columnSize.get(this); column++) {
+                _gameBoard.get(this)[row][column] = ' ';
             }
         }
     }
 
     getRowSize() {
-        return this.rowSize;
+        return _rowSize.get(this);
     }
 
     getColumnSize() {
-        return this.columnSize;
+        return _columnSize.get(this);
     }
 
     addMove(row, column, move) {
-        this.gameBoard[row][column] = move;
+        _gameBoard.get(this)[row][column] = move;
     }
     getMove(row, column) {
-        return this.gameBoard[row][column];
+        return _gameBoard.get(this)[row][column];
     }
 
     getGameBoardState() {
-        return this.gameBoard;
+        return _gameBoard.get(this);
     }
-
-
-
-
-
-
-
-
 
 }
 
