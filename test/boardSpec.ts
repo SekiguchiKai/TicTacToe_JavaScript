7636b461c7db556e25a3c446ac33c71b1eb05c7e
SekiguchiKai <sekiguchikai@SekiguchiKai-1.local>
2017-02-13 16:11:49 +0900

    boardSpec.tsをBDD仕様に変更

--- test/boardSpec.ts
+++ test/boardSpec.ts
@@ -10,19 +10,15 @@ export function boardSpec() {
     let rowIdxArray = [0, 1, 2];
     let columnIdxArray = [0, 1, 2];
 
-    describe('bordクラスのgameBoard配列に対して、打ち手を格納すると正しく格納され、取得することができる', () => {
+    describe('bordクラスのgameBoard配列の', () => {
         // 二次元配列のforEach
         rowIdxArray.forEach((row) => {
             columnIdxArray.forEach((column) => {
-                testPutMoveAndgetMove(row, column, MOVE.CIRCLE, MOVE.CIRCLE);
+                it(`row:${row}, column:${column}に対して、打ち手${MOVE.CIRCLE}を格納すると正しく格納され、取得することができる`, () => {
+                    board.putMove(row, column, MOVE.CIRCLE);
+                    assert(board.getMove(row, column) === MOVE.CIRCLE);
+                });
             })
         })
-        // putMoveとgetMoveメソッドが正常に動作するかを確認するための関数
-        function testPutMoveAndgetMove(row: number, column: number, data: string, expected: string) {
-            it(`gameBoard配列に対して、row:${row}, column:${column}に対して、打ち手${data}を格納すると正しく格納され、取得することができる`, () => {
-                board.putMove(row, column, data);
-                assert(board.getMove(row, column) === expected);
-            });
-        }
-    });
+    })
 }
