0284c66a5987bbcf9f339992b854e16ff1b3df02
SekiguchiKai <sekiguchikai@SekiguchiKai-1.local>
2017-02-13 17:10:50 +0900

    cpuSpec.tsをBDD仕様に変更

--- test/cpuSpec.ts
+++ test/cpuSpec.ts
@@ -4,112 +4,84 @@ import Cpu from '../src/ts/cpu';
 import { MOVE } from '../src/ts/const';
 
 export function cpuSpec() {
-    describe('cpuクラスのdoMoveメソッドで、適切な場所に打ち手を打つ', () => {
-        checkRow(MOVE.CIRCLE);
-        checkColumn(MOVE.CIRCLE);
-        checkLeftSlantingLine(MOVE.CIRCLE);
-        checkRightSlantingLine(MOVE.CIRCLE);
+    describe('cpuクラスのdoMoveメソッド', () => {
+        let board: Board;
+        const rowSize = 3;
+        const columnSize = 3;
+        let cpu: Cpu;
 
+        beforeEach(() => {
+            board = new Board(rowSize, columnSize);
+            cpu = new Cpu();
+        });
 
-        /**
-         * rowに対して、doMoveメソッドが適切に機能しているかを確認するための関数
-         */
-        function checkRow(move: string) {
-
-            const rowSize = 3;
-            const columnSize = 3;
-
-            let board = new Board(rowSize, columnSize);
+        it('rowに対して2つの相手の打ち手を与えると、3つ揃うのを阻止する', () => {
             let array = [];
 
             for (let row = 0; row < 3; row++) {
                 for (let column = 0; column < 2; column++) {
-                    board.putMove(row, column, move);
+                    board.putMove(row, column, MOVE.CIRCLE);
                     array.push(row, column);
                 }
                 const expectedRow = row;
                 const expectedColumn = 2;
 
-                testdoMove(board, expectedRow, expectedColumn, `rowのチェック[row: ${array[0]}, column: ${array[1]}] = ${move}, [row: ${array[2]}, column: ${array[3]}] = ${move} の時にtestdoMoveIは[row: ${expectedRow}, column: ${expectedColumn}]`);
+                const bestCell = cpu.doMove(4, board);
+                assert(bestCell['rowVal'] === expectedRow);
+                assert(bestCell['columnVal'] === expectedColumn);
                 array.length = 0;
                 board = new Board(rowSize, columnSize);
             }
-        }
+        });
 
-
-        /**
-         * columnに対して、doMoveメソッドが適切に機能しているかを確認するための関数
-         */
-        function checkColumn(move: string) {
-
-            const rowSize = 3;
-            const columnSize = 3;
-
-            let board = new Board(rowSize, columnSize);
+        it('columnに対して2つの相手の打ち手を与えると、3つ揃うのを阻止する', () => {
             let array = [];
 
             for (let column = 0; column < 3; column++) {
                 for (let row = 0; row < 2; row++) {
-                    board.putMove(row, column, move);
+                    board.putMove(row, column, MOVE.CIRCLE);
                     array.push(row, column);
                 }
                 const expectedRow = 2;
                 const expectedColumn = column;
 
-                testdoMove(board, expectedRow, expectedColumn, `rowのチェック[row: ${array[0]}, column: ${array[1]}] = ${move}, [row: ${array[2]}, column: ${array[3]}] = ${move} の時にtestdoMoveIは[row: ${expectedRow}, column: ${expectedColumn}]`);
+                const bestCell = cpu.doMove(4, board);
+                assert(bestCell['rowVal'] === expectedRow);
+                assert(bestCell['columnVal'] === expectedColumn);
+
                 array.length = 0;
                 board = board = new Board(rowSize, columnSize);
             }
-        }
+        });
 
-        /**
-         * 左斜めのラインに対して、doMoveメソッドが適切に機能しているかを確認するための関数
-         */
-        function checkLeftSlantingLine(move: string) {
-            const rowSize = 3;
-            const columnSize = 3;
-            let board = new Board(rowSize, columnSize);
-
-            board.putMove(0, 0, move);
-            board.putMove(1, 1, move);
+        it('左斜めのラインに対して2つの相手の打ち手を与えると、3つ揃うのを阻止する', () => {
+            board.putMove(0, 0, MOVE.CIRCLE);
+            board.putMove(1, 1, MOVE.CIRCLE);
 
             const expectedRow = 2;
             const expectedColumn = 2;
 
-            testdoMove(board, expectedRow, expectedColumn, `rowのチェック[row: 0, column: 0 = ${move}, [row: 1, column: 1 = ${move} の時にtestdoMoveIは[row: ${expectedRow}, column: ${expectedColumn}]`);
+            const bestCell = cpu.doMove(4, board);
+            assert(bestCell['rowVal'] === expectedRow);
+            assert(bestCell['columnVal'] === expectedColumn);
 
-        }
-        /**
-         * 右斜めのラインに対して、doMoveメソッドが適切に機能しているかを確認するための関数
-         */
-        function checkRightSlantingLine(move: string) {
+        });
+
+        it('右斜めのラインに対して2つの相手の打ち手を与えると、3つ揃うのを阻止する', () => {
             const rowSize = 3;
             const columnSize = 3;
             let board = new Board(rowSize, columnSize);
 
-            board.putMove(0, 2, move);
-            board.putMove(1, 1, move);
+            board.putMove(0, 2, MOVE.CIRCLE);
+            board.putMove(1, 1, MOVE.CIRCLE);
 
             const expectedRow = 2;
             const expectedColumn = 0;
 
-            testdoMove(board, expectedRow, expectedColumn, `rowのチェック[row: 0, column: 2 = ${move}, [row: 1, column: 1 = ${move} の時にtestdoMoveIは[row: ${expectedRow}, column: ${expectedColumn}]`);
-        }
 
-
-        /**
-         * doMoveメソッドが適切に機能しているかを確認するための関数
-         */
-        function testdoMove(board: Board, expectedRow: number, expectedColumn: number, comment: string) {
-            const cpu = new Cpu();
             const bestCell = cpu.doMove(4, board);
-
-            it(comment, () => {
-
-                assert(bestCell['rowVal'] === expectedRow);
-                assert(bestCell['columnVal'] === expectedColumn);
-            })
-        }
-
+            assert(bestCell['rowVal'] === expectedRow);
+            assert(bestCell['columnVal'] === expectedColumn);
+        });
     });
-};
+}
